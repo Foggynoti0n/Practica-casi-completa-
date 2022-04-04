09ab58aa74ee442cc39d73db1ec41bafe01ac296
Ludmila <lurm98@gmail.com>
2022-04-04 10:50:30 -0300

    ...

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let menu = document.querySelector("#menu");
+let toggleIcon = document.querySelector("#toggle-icon");
+
+toggleIcon.addEventListener('click', function(){
+ menu.classList.toggle("menuOn")
+});
